6e9dae37429cf81791c01f3bce3a9997e974b7e5
Faith <f.elikirmawi@gmail.com>
2020-02-10 11:17:22 +0000

    added css and js files

--- .eleventy.js
+++ .eleventy.js
@@ -24,3 +24,14 @@ module.exports = function(config) {
   };
 
 };
+
+module.exports = function(eleventyConfig) {
+  // Output directory: _site
+
+  // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy("img");
+  // Copy `css/fonts/` to `_site/css/fonts`
+  
+  // If you use a subdirectory, it'll copy using the same directory structure.
+  eleventyConfig.addPassthroughCopy("css/fonts");
+};
